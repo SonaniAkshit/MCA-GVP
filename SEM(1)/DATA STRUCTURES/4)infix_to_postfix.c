b47fcf9d9c8027891b4578ae8c602cf44a645522
Akshit Patel <sonaniakshit684@gmail.com>
2024-12-13 06:08:01 +0530

    Update 4)infix_to_postfix.c

--- SEM(1)/DATA STRUCTURES/4)infix_to_postfix.c
+++ SEM(1)/DATA STRUCTURES/4)infix_to_postfix.c
@@ -1,57 +1,20 @@
-/*Write program to convert infix expression to postfix expression using stack*/
 #include<stdio.h>
 #include<conio.h>
 
-int operatorPriority(char operatorValue) {
-    int priority;
-    if (operatorValue == '*' || operatorValue == '/') {
-        priority = 2;
-    } 
-	else if (operatorValue == '+' || operatorValue == '-') {
-        priority = 1;
-    }
-    return priority;
+int operatorpriority(char operatorvalue){
+	if(operatorvalue == '*' || operatorvalue == '/')
+		return 2;
+	else if(opratorvalue == '+' || operatorvalue == '-')
+		return 1;
+	else
+		return 0;
 }
-
-void push(char opeArray[], int *top, char operatorValue) {
-    *top += 1;
-    opeArray[*top] = operatorValue;
+void push(char opearray[],int *top,char operatorvalue){
+	*top +=1;
+	opearray[*top]=operatorvalue;
 }
-
-char pop(char opeArray[], int *top) {
-    char value = opeArray[*top];
-    *top -= 1;
-    return value;
+char pop(char opearray[],int *top){
+	char value=opearray[*top];
+	*top -=1;
+	return value;
 }
-
-void main() {
-    char opeArray[50], ch[20], operatorPop;
-    int i = 0, x = -1, *top = &x;
-
-    printf("Enter the Infix Expression: ");
-    scanf("%s", ch);
-
-    while (ch[i] != '\0') {
-        if (ch[i] == '+' || ch[i] == '-' || ch[i] == '*' || ch[i] == '/') {
-            if (*top == -1) {
-                push(opeArray, top, ch[i]);
-            } 
-			else {
-                while (*top >= 0 && operatorPriority(opeArray[*top]) >= operatorPriority(ch[i])) {
-                    operatorPop = pop(opeArray, top);
-                    printf("%c", operatorPop);
-                }
-                push(opeArray, top, ch[i]);
-            }
-        } 
-		else if ((ch[i] >= 'a' && ch[i] <= 'z') || (ch[i] >= 'A' && ch[i] <= 'Z')) {
-            printf("%c", ch[i]);
-        }
-        i++;
-    }
-    while (*top != -1) {
-        operatorPop = pop(opeArray, top);
-        printf("%c", operatorPop);
-    }
-}
-
